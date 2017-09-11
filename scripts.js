b4b393f01da1e59c7c02f55e59f7e4683d6b1825
Attila Hegedues <easygater@gmail.com>
2017-09-12 01:56:59 +0200

    Re-designed the search box with glass and cross icon:

--- scripts.js
+++ scripts.js
@@ -1,4 +1,35 @@
+(function ($) {
+    $.fn.replaceClass = function (pFromClass, pToClass) {
+        return this.removeClass(pFromClass).addClass(pToClass);
+    };
+}(jQuery));
+
+function startSearch(event) {
+  if(event.key === 'Enter') {
+    var searchFor = $('.search-box:text').val();
+    $('.search-box:text').val('');
+    $('i').replaceClass('fa-times', 'fa-search');
+    $('.search-box').blur();
+
+    $.ajax({});
+
+    console.log('Key enterred: ' + event.key);
+    console.log('enterred text = ' + searchFor);
+  }
+}
+
+$('.search-box').focus(function() {
+  $('i').replaceClass('fa-search', 'fa-times');
+});
+
+$('.search-box').blur(function() {
+  $('i').replaceClass('fa-times', 'fa-search');
+});
+
+$('i.fa').click(function() {
+  $('.search-box:text').val('');
+});
+
 $(document).ready(function() {
   console.log('Ziggy is ready!');
-  // $('body').append('<h3>Ziggy is ready!</h3>');
 });
