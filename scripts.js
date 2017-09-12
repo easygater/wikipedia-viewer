7c15b57c1523f5357ea7ef5dbc4d8c9a9c74a1d1
Attila Hegedues <easygater@gmail.com>
2017-09-12 14:00:17 +0200

    Genereated ajax call to wiki api and prepared display function.

--- scripts.js
+++ scripts.js
@@ -4,6 +4,17 @@
     };
 }(jQuery));
 
+function displaySearchResults(items) {
+  // console.log('Ziggy made ajax with item count ' + items.length);
+  // console.log('Title -> ' + items[0].title);
+  // console.log('Description -> ' + items[0].snippet);
+  if(items.length > 0) {
+    for(item in items) {
+      // TODO: write the code to display each item on the site
+    }
+  } else {}
+}
+
 function startSearch(event) {
   if(event.key === 'Enter') {
     var searchFor = $('.search-input:text').val();
@@ -11,7 +22,25 @@ function startSearch(event) {
     $('i').replaceClass('fa-times', 'fa-search');
     $('.search-input').blur();
 
-    // $.ajax({});
+    /*
+      url: 'https://en.wikipedia.org/w/api.php'   -> works
+      url: 'https://meta.wikimedi.org/w/api.php'  -> fails
+    */
+    $.ajax({
+      url: 'https://en.wikipedia.org/w/api.php',
+      data: {
+        action: 'query',
+        list: 'search|allpages',
+        titles: 'Main Page',
+        srsearch: searchFor,
+        srlimit: 100,
+        format: 'json'
+      },
+      dataType: 'jsonp',
+      success: function(data) {
+        displaySearchResults(data.query.search);
+      }
+    });
 
     console.log('Key enterred: ' + event.key);
     console.log('enterred text = ' + searchFor);
